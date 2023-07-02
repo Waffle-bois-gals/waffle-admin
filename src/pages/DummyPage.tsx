0a21014d045f179131e79f7b338eaf70e03dd354
Czeglédi Levente <czegledi.levente@wadjet.hu>
2023-07-02 14:18:01 +0200

    trigger pr action one more time

--- src/pages/DummyPage.tsx
+++ src/pages/DummyPage.tsx
@@ -3,7 +3,7 @@ import { FC } from "react";
 const DummyPage: FC = () => {
   return (
     <div className="flex h-[100vh] w-full bg-amber-100 items-center justify-center">
-      Bonjour
+      Szasztok bigék!
     </div>
   );
 };
