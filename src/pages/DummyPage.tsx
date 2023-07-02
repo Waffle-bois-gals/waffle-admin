f63f39b30a461bda67c60f36ae38bcae15d9bd0b
Czeglédi Levente <czegledi.levente@wadjet.hu>
2023-07-02 14:02:10 +0200

    fix tailwind not working

--- src/pages/DummyPage.tsx
+++ src/pages/DummyPage.tsx
@@ -2,7 +2,7 @@ import { FC } from "react";
 
 const DummyPage: FC = () => {
   return (
-    <div className="flex h-full w-full bg-amber-100 items-center justify-center">
+    <div className="flex h-[100vh] w-full bg-amber-100 items-center justify-center">
       Bonjour
     </div>
   );
